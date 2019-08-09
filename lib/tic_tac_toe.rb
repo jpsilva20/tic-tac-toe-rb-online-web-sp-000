b7c1ed48b0d76e5704f5bfe82d036dc9033b71bf
Jennifer Silva <jennifersilva20@gmail.com>
2019-08-09 07:04:27 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -17,9 +17,31 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(user_input)
+  user_input.to_i - 1
+end 
+
+def move(board, index, player)
+  board[index] = player
+end 
+
+def position_taken?(board, index)
+  board[index] == "X" or board[index] == "O"
+end 
+
+
 def valid_move?(board, index)
   if index.between?(0,8) && !position_taken?(board, index)
 end
+
+def turn_count(board)
+  board.count {|token| token == "X" or token == "O"}
+end 
+
+def current_player(board)
+  turn_count(board).even? ? "X" : "O"
+end
+
 
 def won?(board)
   WIN_COMBINATIONS.detect do |combo|
@@ -41,38 +63,17 @@ def over?(board)
   won?(board) or draw?(board)
 end 
 
-def input_to_index(user_input)
-  user_input.to_i - 1
-end 
-
-def turn(board)
-  puts "Please enter 1-9:"
-  user_input = gets.strip
-  index = input_to_index(user_input)
-  if valid_move?(board, index)
-    move(board, index, current_player(board))
-    display_board(board)
-  else 
-    turn(board)
-  end 
-end 
-
-def position_taken?(board, index)
-  board[index] == "X" or board[index] == "O"
-end 
-
-def current_player(board)
-  turn_count(board).even? ? "X" : "O"
-end
-
-def turn_count(board)
-  board.count {|token| token == "X" or token == "O"}
-end 
 
 
-def move(board, index, player)
-  board[index] = player
-end 
+
+
+
+
+
+
+
+
+
 
 def winner(board)
   if winning_combo = won?(board)
