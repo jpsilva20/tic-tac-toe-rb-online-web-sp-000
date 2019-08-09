f29d328a08d223b63ec07a8f1683552812269516
Jennifer Silva <jennifersilva20@gmail.com>
2019-08-09 06:58:16 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -26,7 +26,7 @@ def won?(board)
     board[combo[0]] == board[combo[1]] &&
       board[combo[1]] == board[combo[2]] &&
       position_taken?(board, combo[0])
-    end
+  end
 end 
 
 def full?(board)
