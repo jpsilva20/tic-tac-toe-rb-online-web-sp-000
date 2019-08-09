82f5ee9b1d386256df9f21fef2d384e46543dfd3
Jennifer Silva <jennifersilva20@gmail.com>
2019-08-09 07:22:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -24,13 +24,15 @@ end
 
 
 def valid_move?(board, index)
-  if index.between?(0,8) && !position_taken?(board, index)
-    return true 
-  end 
-  else
-    return false
-  end 
+  if index.between?(1,9)
+    if !position_taken?(board, index)
+      return  true
+    end
+  end
+
+  index.between?(0,8) && !position_taken?(board, index)
 end
+
 
 def turn_count(board)
   turn = 0 
