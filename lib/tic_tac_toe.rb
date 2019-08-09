74d2362426f05d47bde71a24bf86edecf911ce1c
Jennifer Silva <jennifersilva20@gmail.com>
2019-08-09 06:52:18 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,22 +23,11 @@ def valid_move?(board, index)
 end
 
 def won?(board)
-  WIN_COMBINATIONS.each {|win_combination|
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
-    
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
-    
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combination
-    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combination
-    end    
-  }
-  return false 
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+      board[combo[1]] == board[combo[2]] &&
+      position_taken(board, combo[0])
+    end
 end 
 
 def full?(board)
